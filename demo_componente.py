757744ba175d76bfcd99ed999b40ecf54473df18
Alejandro Ogalla López <92046192+amogalla@users.noreply.github.com>
2021-12-15 20:59:06 +0100

    Código de demo simplificado

--- demo_componente.py
+++ demo_componente.py
@@ -1,36 +1,13 @@
 import sys
-from PySide6.QtWidgets import QApplication, QHBoxLayout, QMainWindow, QVBoxLayout, QWidget
-from componente import Boton, Cuadro
-
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.resize(50, 250)
-        layoutCompleto = QVBoxLayout()
-        layoutBotones = QHBoxLayout()
-
-        self.boton1 = Boton("Transparente")
-        self.boton2 = Boton("Opaco")
-        self.cuadro = Cuadro()
-
-        layoutBotones.addWidget(self.boton1)
-        layoutBotones.addWidget(self.boton2)
-        layoutCompleto.addWidget(self.cuadro)
-
-        layoutCompleto.addLayout(layoutBotones)
-
-        contenedor = QWidget()
-        contenedor.setLayout(layoutCompleto)
-        contenedor.resize(100, 100) #Tamaño inicial del contenedor.
-        self.setCentralWidget(contenedor)
-        
-        self.cuadro.setStyleSheet("background-color:red;border-radius:15px;")
-        self.cuadro.resize(100, 100)
-        
-        self.boton1.clicked.connect(self.cuadro.hacer_transparente)
-        self.boton2.clicked.connect(self.cuadro.hacer_opaco)
+from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
+from componente import Ventana
 
 app = QApplication(sys.argv)
-w = Window()
+w = QWidget()
+
+ventana = Ventana()
+w.setLayout(QVBoxLayout())
+w.layout().addWidget(ventana)
+
 w.show()
 app.exec()
