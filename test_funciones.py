54a554ea6e219f0d30385d914b0da4ffb5c36cdd
Alejandro Ogalla López <92046192+amogalla@users.noreply.github.com>
2021-12-28 21:41:43 +0100

    Update test_funciones.py

--- test_funciones.py
+++ test_funciones.py
@@ -2,11 +2,10 @@ import unittest, pytest, sys
 from PySide6 import QtCore
 from PySide6.QtCore import QSize
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QWidget
 
 from componente import Boton, Cuadro, Ventana
 
-#OK
 class BotonTestCase(unittest.TestCase):
     def test_sizeHintCuadro(self):
         if not QtWidgets.QApplication.instance():
@@ -26,65 +25,26 @@ class BotonTestCase(unittest.TestCase):
         result = Boton().sizeHint()
         self.assertEqual(result, QSize(40, 40))
 
+    def test_textoBotonInicial(self):
+        result = Boton().getTexto()
+        self.assertEqual(result, "Pulsa aquí")
 
-#Constructor inicial de un botón
-@pytest.fixture
-def boton(qtbot):
-    botonInicial = Boton()
-    qtbot.addWidget(botonInicial)
+    def test_textoBotonTransparente(self):
+        result = Boton("Transparente").getTexto()
+        self.assertEqual(result, "Transparente")
 
-    return botonInicial
-
-def test_texto_boton_inicial(boton):
-    assert boton.getTexto() == "Pulsa aquí"
-
-#Botón transparente
-@pytest.fixture
-def transparente(qtbot):
-    botonTransparente = Boton("Transparente")
-    qtbot.addWidget(botonTransparente)
-
-    return botonTransparente
-
-def test_texto_boton_transparente(transparente):
-    assert transparente.getTexto() == "Transparente"
-
-
-#Botón opaco
-@pytest.fixture
-def opaco(qtbot):
-    botonOpaco = Boton("Opaco")
-    qtbot.addWidget(botonOpaco)
-
-    return botonOpaco
-
-def test_texto_boton_opaco(opaco):
-    assert opaco.getTexto() == "Opaco"
-
-#OK
-@pytest.fixture
-def app2(qtbot):
-    test_componente = Ventana()
-    qtbot.addWidget(test_componente)
-
-    return test_componente
-
-def test_opacidad_inicial(app2):
-    assert app2.cuadro.getOpacidad() == 1.0
-
-
-
-@pytest.fixture
-def app3(qtbot):
-    test_ventana = Ventana()
-    qtbot.addWidget(test_ventana)
-
-    return test_ventana
-
-
-def test_opacidad_tras_click_opaco(app3, qtbot):
-    qtbot.mouseClick(app3.botonOpaco, QtCore.Qt.LeftButton)
-    assert app3.cuadro.getOpacidad() == 1.0
+    def test_textoBotonOpaco(self):
+        result = Boton("Opaco").getTexto()
+        self.assertEqual(result, "Opaco")
+    
+    def test_opacidadInicial(self):
+        if not QtWidgets.QApplication.instance():
+            app = QtWidgets.QApplication(sys.argv)
+        else:
+            app = QtWidgets.QApplication.instance()
+        result = Cuadro().getOpacidad()
+        print(result)
+        self.assertEqual(result, 1.0)
 
 
 if __name__ == '__main__':
